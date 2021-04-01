2831845d91e350a3f4c0858f5637c3aedd0e1378
NihilFrost <werfoster@yandex.ru>
2021-04-01 05:07:23 +0300

    first work commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    var divLine = $(".line");
+
+    $(".line").on("click", function(event) {
+        // Клик по блоку фишек
+        console.log(event);
+    })
+
+
+});
